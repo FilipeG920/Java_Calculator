ddd77099841f73ec0f22879836781a1f39d1aad6
FilipeG920 <genesisfilipe11@gmail.com>
2024-08-21 14:59:00 -0300

    Insert calculator functions along more GUI rework
    Later I'll visit this project to add more functionalities and create an
    executable.

--- calculator/src/calculator/MyFrame.java
+++ calculator/src/calculator/MyFrame.java
@@ -1,34 +1,25 @@
 package calculator;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-	JTextField textField;
-	JButton[] numberButtons = new JButton[10];
-	JButton[] functionButtons = new JButton[8];
+	private JTextField textField;
+	private JButton[] numberButtons = new JButton[10];
+	private JButton[] functionButtons = new JButton[9];
 	
-	JButton buttonSUM, buttonSUB, buttonMUL, buttonDIV; 
-	JButton decButton, equButton, delButton, clrButton;
-	Font myFont = new Font("Roboto", Font.BOLD,20);
-	JLabel label;
+	private JButton buttonSUM, buttonSUB, buttonMUL, buttonDIV; 
+	private JButton decButton, equButton, delButton, clrButton, negButton;
+	private Font myFont = new Font("Roboto", Font.BOLD,20);
 	
-	double num1 = 0, num2 = 0;
+	double num1 = 0, num2 = 0, result = 0;
 	char operator;
 	
 	MyFrame(){
 		textField = new JTextField();
-		textField.setBounds(10, 25, 410, 150);
+		textField.setBounds(10, 25, 410, 40);
 		textField.setBackground(Color.white);
 		textField.setFont(myFont);
 		textField.setFocusable(false);
@@ -36,8 +27,7 @@ public class MyFrame extends JFrame implements ActionListener {
 		
 		// Creating the buttons of numbers, buttons of functions and its panels
 		JPanel panel = new JPanel(new GridLayout(4, 4, 0, 0));
-		//panel.setPreferredSize(new Dimension(250, 250));
-		panel.setBounds(10, 200, 250, 250);
+		panel.setBounds(10, 80, 250, 250);
 		panel.setBackground(Color.lightGray);
 		
 		buttonSUM = new JButton("+");
@@ -48,6 +38,7 @@ public class MyFrame extends JFrame implements ActionListener {
 		equButton = new JButton("=");
 		delButton = new JButton("DELETE");
 		clrButton = new JButton("CLEAR");
+		negButton = new JButton("NEGATIVE");
 		
 		functionButtons[0] = buttonSUM;
 		functionButtons[1] = buttonSUB;
@@ -57,6 +48,7 @@ public class MyFrame extends JFrame implements ActionListener {
 		functionButtons[5] = equButton;
 		functionButtons[6] = delButton;
 		functionButtons[7] = clrButton;
+		functionButtons[8] = negButton;
 		
 		for (int i = 0; i < numberButtons.length; i++) {
 			numberButtons[i] = new JButton(String.valueOf(i));
@@ -88,8 +80,9 @@ public class MyFrame extends JFrame implements ActionListener {
 		panel.add(equButton);
 		panel.add(buttonDIV);
 
-		delButton.setBounds(270, 200, 120, 50);
-		clrButton.setBounds(270, 260, 120, 50);
+		delButton.setBounds(270, 80, 140, 50);
+		clrButton.setBounds(270, 140, 140, 50);
+		negButton.setBounds(270, 200, 140, 50);
 		
 		this.setTitle("Calculator");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -98,6 +91,7 @@ public class MyFrame extends JFrame implements ActionListener {
 		this.add(textField);
 		this.add(delButton);
 		this.add(clrButton);
+		this.add(negButton);
 		this.add(panel);
 		this.setVisible(true);
 		//this.setResizable(false);
@@ -106,6 +100,79 @@ public class MyFrame extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
+		for (int i = 0; i < 10; i++) {
+			if (e.getSource() == numberButtons[i]) {
+				textField.setText(textField.getText().concat(String.valueOf(i)));
+ 			}
+		}
+		
+		if (e.getSource() == decButton) {
+			textField.setText(textField.getText().concat(String.valueOf(".")));
+		}
+		
+		if (e.getSource() == buttonSUM) {
+			num1 = Double.parseDouble(textField.getText());
+			operator = '+';
+			textField.setText("");
+		}
+		
+		if (e.getSource() == buttonSUB) {
+			num1 = Double.parseDouble(textField.getText());
+			operator = '-';
+			textField.setText("");		
+		}
+		
+		if (e.getSource() == buttonMUL) {
+			num1 = Double.parseDouble(textField.getText());
+			operator = 'x';
+			textField.setText("");		
+		}
+		
+		if (e.getSource() == buttonDIV) {
+			num1 = Double.parseDouble(textField.getText());
+			operator = '/';
+			textField.setText("");		
+		}
+		
+		if (e.getSource() == equButton) {
+			num2 = Double.parseDouble(textField.getText());
+			
+			switch(operator) {
+				case '+':
+					result = num1 + num2;
+					break;
+				case '-':
+					result = num1 - num2;
+					break;
+				case 'x':
+					result = num1 * num2;
+					break;
+				case '/':
+					result = num1 / num2;
+					break;
+			}
+			
+			textField.setText(String.valueOf(result));
+			num1 = result;
+		}
+		
+		if (e.getSource() == clrButton) {
+			textField.setText("");
+		}
+		
+		if (e.getSource() == delButton) {
+			String string = textField.getText();
+			textField.setText("");
+			for (int i = 0; i < string.length() - 1; i++){
+				textField.setText(textField.getText() + string.charAt(i));
+			}
+		}
+		
+		if (e.getSource() == negButton) {
+			double temp = Double.parseDouble(textField.getText());
+			temp *= -1;
+			textField.setText(String.valueOf(temp));
+		}
 	}
 
 }
